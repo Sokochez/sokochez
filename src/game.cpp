21c3de9a398dec2f3b97de3b0062355b1a2aa1cd
Baptiste Pouget <baba@firemail.cc>
2016-12-18 18:48:08 +0100

    Added number of moves and a message

--- src/game.cpp
+++ src/game.cpp
@@ -15,15 +15,26 @@ void Game (const string & FileName)
         cout << "Error : Map not valid" << endl;
         return;
     }
-    ShowMatrix(map);
     CPosition P1 (FindPlayer(map,KTokenPlayer1));
     CPosition P2 (FindPlayer(map, KTokenPlayer2));
     char key;
+    unsigned nbmoves (0);
+    ClearScreen();
+    cout << "Loaded map " << FileName << endl
+         << "Win by joining the other player" << endl
+         << "Press a key to begin" << endl;
+    getchar();
+
     while (!CheckWin(P1, P2))
     {
         ClearScreen();
         ShowMatrix(map);
         cin >> key;
         Action(map, key, P1, P2);
+        ++nbmoves;
     }
+    ClearScreen();
+    cout << "Victory ! You won in "<< nbmoves << " moves !" << endl;
+    getchar();
+    getchar();
 }
